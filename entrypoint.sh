5b8340b49c8d4c2d7e27ea03007fb107ec05c079
Jeremy Kongs <jeremy.kongs@maximintegrated.com>
2020-10-07 16:54:17 -0500

    Change docker file, from alpine to ubuntu 18.04

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,7 @@ time=$(date)
 echo "::set-output name=time::$time"
 
 echo "Attempting to run $PATH_TO_RUN_FILE"
-$(file $PATH_TO_RUN_FILE)
-$(cat $PATH_TO_RUN_FILE)
+file $PATH_TO_RUN_FILE
+cat $PATH_TO_RUN_FILE
 file_run_status=$($PATH_TO_RUN_FILE)
 echo "::set-output name=file_run_status::$file_run_status"
