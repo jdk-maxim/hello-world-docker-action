3e8d5946fbc8089541f25c724445eef014447b2f
Jeremy Kongs <jeremy.kongs@maximintegrated.com>
2020-10-07 16:58:40 -0500

    Change from ubunutu to Tensorflow docker
    Also remove a bit of debug from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,15 +6,13 @@ PATH_TO_RUN_FILE=$PATH_TO_WORKSPACE$2
 echo "ls of workspace"
 ls $PATH_TO_WORKSPACE
 
-cat /github/workspace/README.md
-file /github/workspace/push_actions.sh
+#cat /github/workspace/README.md
 
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
 
 echo "Attempting to run $PATH_TO_RUN_FILE"
-file $PATH_TO_RUN_FILE
-cat $PATH_TO_RUN_FILE
+#cat $PATH_TO_RUN_FILE
 file_run_status=$($PATH_TO_RUN_FILE)
 echo "::set-output name=file_run_status::$file_run_status"
