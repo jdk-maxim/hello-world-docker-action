e821d1ccd477c6488ebf53dec969bab626d016e6
Jeremy Kongs <jeremy.kongs@maximintegrated.com>
2020-10-07 16:27:00 -0500

    Use the correct path to the workspace

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh -l
 
+PATH_TO_WORKSPACE = "/github/workspace/"
+PATH_TO_RUN_FILE = $PATH_TO_WORKSPACE$2
+
 echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
 
-echo "Attempting to run $2"
-file_run_status=$($2)
+echo "Attempting to run $PATH_TO_RUN_FILE"
+file_run_status=$(PATH_TO_RUN_FILE)
 echo "::set-output name=file_run_status::$file_run_status"
