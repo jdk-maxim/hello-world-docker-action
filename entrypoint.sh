923b97ac9d644becee893de73d6a35c94442ece9
Jeremy Kongs <jeremy.kongs@maximintegrated.com>
2020-10-07 17:29:47 -0500

    Return status not output

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,5 +14,6 @@ echo "::set-output name=time::$time"
 
 echo "Attempting to run $PATH_TO_RUN_FILE"
 #cat $PATH_TO_RUN_FILE
-file_run_status=$($PATH_TO_RUN_FILE)
+$PATH_TO_RUN_FILE
+file_run_status=$?
 echo "::set-output name=file_run_status::$file_run_status"
