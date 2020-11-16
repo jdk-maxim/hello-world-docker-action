6ae47255eb039c59b3b4b0e03bdcf998cf99244d
Jeremy Kongs <jeremy.kongs@maximintegrated.com>
2020-11-16 13:53:15 -0600

    Add some debug ls

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 PATH_TO_WORKSPACE="/github/workspace/"
 PATH_TO_RUN_FILE=$PATH_TO_WORKSPACE$1
 
+ls -la $PATH_TO_WORKSPACE
+ls -la /github/workspace/.github/regression_test_scripts
+
 echo "Attempting to run $PATH_TO_RUN_FILE"
 $PATH_TO_RUN_FILE
 file_run_status=$?
